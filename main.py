d820a3f19aabc3b50b8bcae9f55422d1aa06f7fb
Doffy <122683480+userDoffy@users.noreply.github.com>
2024-05-18 05:20:20 +0545

    another commit

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def track_order(parameters:dict,session_id:str):
     order_status=db_helper.get_order_status(order_id)
     
     if order_status:
-        fulfillment_text=f"The order status for order id: {order_id} is:{order_status}"
+        fulfillment_text=f"The order status for order id: {order_id} is: {order_status}"
     else:
         fulfillment_text=f"No order found with order id: {order_id}"
 
